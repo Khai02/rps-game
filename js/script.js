e8eb559a0a339fb0da0ae81c72645ff85d013f1b
Khai02 <92036912+Khai02@users.noreply.github.com>
2024-05-19 14:21:42 +0100

    Finished writing the logic for the round outcomes

--- js/script.js
+++ js/script.js
@@ -16,8 +16,20 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === 'rock' && computerChoice === 'rock') {
     console.log("You both drew rock, it's a tie!")
+  } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+    console.log("You lose! Their paper covered your rock!")
+  } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    console.log("You win! Your rock shattered their scissors!")
+  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log("You win! Your paper covered their rock")
   } else if (humanChoice === 'paper' && computerChoice === 'paper') {
     console.log("You both drew paper, it's a tie!")
+  } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+    console.log("You lose! Their scissors cut your paper into pieces!")
+  } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+    console.log("You lose! Their rock shattered your scissors!")
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    console.log("You win! Your scissors cut their paper into pieces!")
   } else if (humanChoice === 'scissors' && computerChoice === 'scissors')
     console.log("You both drew scissors, it's a tie!")
 }
