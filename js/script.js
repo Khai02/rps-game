0ab2d453a0492a6fe291b3c690ab496e4864c7d6
Khai02 <92036912+Khai02@users.noreply.github.com>
2024-05-19 13:24:36 +0100

    Created functions to get rock, paper or scissors from the computer and user, also wrote the logic for draw cases

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+  const computerChoice = ['rock', 'paper', 'scissors'];
+  index = Math.floor(Math.random() * 3);  
+  return computerChoice[index];
+}
+
+function getHumanChoice() {
+  const choice = ['rock', 'paper', 'scissors'];
+  let humanChoice = prompt('Please choose between: rock, paper or scissors:').toLowerCase();
+  while (!choice.includes(humanChoice)) {
+    humanChoice = prompt("Uh Oh! Please enter rock, paper, or scissors:").toLowerCase();
+  }
+  return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === 'rock' && computerChoice === 'rock') {
+    console.log("You both drew rock, it's a tie!")
+  } else if (humanChoice === 'paper' && computerChoice === 'paper') {
+    console.log("You both drew paper, it's a tie!")
+  } else if (humanChoice === 'scissors' && computerChoice === 'scissors')
+    console.log("You both drew scissors, it's a tie!")
+}
+
+let humanScore = 0
+let computerScore = 0
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
